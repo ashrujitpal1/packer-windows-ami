a0a24ace534373d82056b003af757cfb728e0a74
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-13 19:02:04 +0530

    first commit

--- asg complete/asg/launch-configuration.tf
+++ asg complete/asg/launch-configuration.tf
@@ -1,17 +1,8 @@
 resource "aws_launch_configuration" "demo" {
-    image_id = "ami-0022f774911c1d690"
+    image_id = "ami-0ceb587ab0954eb74"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.web-security.id]
     key_name = "devops-keys"
-
-    user_data = <<-EOF
-                    #!/bin/bash
-                    sudo yum update
-                    sudo curl https://www.rpmfind.net/linux/epel/next/8/Everything/x86_64/Packages/b/busybox-1.35.0-2.el8.next.x86_64.rpm --output busybox.rpm
-                    sudo yum install busybox.rpm iasl -y
-                    echo "You are learning Terraform, Have a good day" > index.html
-                    nohup busybox httpd -f -p 8080 &
-                EOF
     
     lifecycle {
       create_before_destroy = true
--- asg complete/asg/output.tf
+++
@@ -1,5 +0,0 @@
-output "load-balancer-dns-name" {
-    value = aws_lb.demo.dns_name
-    description = "FQDN for alb"
-  
-}
