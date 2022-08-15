36bb800f97eeaa1d3e3ec6fabbb20d74cc728528
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-15 18:49:19 +0530

    ASG

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -24,13 +24,4 @@ resource "aws_launch_template" "demo" {
       Name = "test"
     }
   }
-
-  user_data = <<-EOF
-                    #!/bin/bash
-                    sudo yum update
-                    sudo curl https://www.rpmfind.net/linux/epel/next/8/Everything/x86_64/Packages/b/busybox-1.35.0-2.el8.next.x86_64.rpm --output busybox.rpm
-                    sudo yum install busybox.rpm iasl -y
-                    echo "You are learning Terraform, Have a good day" > index.html
-                    nohup busybox httpd -f -p 8080 &
-                EOF
 }
