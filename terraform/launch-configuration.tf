e7297cf6aad9c1b783155d6596bbd99b270ace46
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-15 16:36:07 +0530

    ASG

--- terraform/launch-configuration.tf
+++ terraform/launch-configuration.tf
@@ -1,4 +1,4 @@
-resource "aws_launch_configuration " "demo" {
+resource "aws_launch_configuration" "demo" {
     image_id = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.web-security.id]
