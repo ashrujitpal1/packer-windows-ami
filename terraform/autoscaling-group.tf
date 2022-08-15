7baf51d0079a86c6fcc952d51d53144a42e20852
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-15 16:29:20 +0530

    ASG

--- terraform/autoscaling-group.tf
+++ terraform/autoscaling-group.tf
@@ -1,5 +1,5 @@
 resource "aws_autoscaling_group" "demo" {
-    launch_template = aws_launch_template.demo.name
+    launch_configuration = aws_launch_configuration.demo.name
     vpc_zone_identifier = ["subnet-0820b2d09794f06a9", "subnet-04169f14f7ee86ab3"]
 
     target_group_arns = [aws_lb_target_group.asg.arn]
